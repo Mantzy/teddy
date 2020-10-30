b97c776725cc85caff622fff6e4db5c8f035241e
Mantzy <fanni.kovacs@hotmail.com>
2020-10-30 21:06:02 +0000

    adding products to cart

--- js/product.js
+++ js/product.js
@@ -5,6 +5,17 @@ let image = document.getElementById('image')
 let teddyName = document.getElementById('teddyName')
 let teddyPrice = document.getElementById('teddyPrice')
 let teddyColor = document.getElementById('teddyColors')
+let product
+let quantity = JSON.parse(localStorage.getItem('quantity'))
+if (quantity == null) {
+    localStorage.setItem('quantity', 0)
+}
+productsInCart = JSON.parse(localStorage.getItem('productsInCart'))
+console.log(productsInCart)
+if (productsInCart == null) {
+    localStorage.setItem('productsInCart', '[]')
+
+}
 
 
 
@@ -14,6 +25,7 @@ request.open('GET', 'http://localhost:3000/api/teddies/' + id, true)
 request.onload = function() {
     // Begin accessing JSON data here
     var data = JSON.parse(this.response)
+    product = data
     if (request.status >= 200 && request.status < 400) {
         image.src = data.imageUrl
         teddyName.textContent = data.name
@@ -35,6 +47,31 @@ request.onload = function() {
         errorMessage.textContent = `The Teddies are hibernating for the winter`
         app.appendChild(errorMessage)
     }
+
+
+
 }
 
 request.send()
+
+// add teddies to cart
+let addButton = document.getElementById('addToCart')
+
+
+
+
+addButton.addEventListener('click', () => {
+    addToCart(product)
+
+})
+
+function addToCart(product) {
+    let productsInCart = []
+    productsInCart = JSON.parse(localStorage.getItem('productsInCart'))
+    productsInCart.push(product)
+    localStorage.setItem('productsInCart', JSON.stringify(productsInCart))
+    let quantity = JSON.parse(localStorage.getItem('quantity')) + 1
+    localStorage.setItem('quantity', JSON.stringify(quantity))
+    let itemNumberInCart = document.getElementById('itemNumberInCart')
+    itemNumberInCart.innerHTML = quantity
+}
